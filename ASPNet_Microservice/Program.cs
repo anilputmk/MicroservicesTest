64f093fb7788e4803150f5ef800a4f31e8094004
anilp <anilp@thoughtworks.com>
2018-08-17 18:29:55 +0530

    Init files adding

---
+++ ASPNet_Microservice/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace ASPNet_Microservice
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //var config = new ConfigurationBuilder().AddCommandLine(args).Build();
+            //var host = new WebHostBuilder().UseKestrel().UseStartup<Startup>().UseConfiguration(config).Build();
+            //host.Run();
+            var config = new ConfigurationBuilder().AddCommandLine(args).Build();
+            var host = new WebHostBuilder().UseKestrel().UseStartup<StartUp>().UseConfiguration(config).Build();
+            host.Run();
+            //CreateWebHostBuilder(args).Build().Run();
+        }
+
+        //public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            //WebHost.CreateDefaultBuilder(args)
+                //.UseStartup<Startup>();
+    }
+}
---
+++ ASPNet_Microservice/Startup.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace ASPNet_Microservice
+{
+    public class StartUp
+    {
+        public StartUp(IHostingEnvironment environment) {
+            
+        }
+
+        public void Configure(IApplicationBuilder application, IHostingEnvironment environment, ILoggerFactory loggerFactory)
+        {
+            application.Run(async (context) => {
+                await context.Response.WriteAsync("Hello World !");
+            });
+        }
+    }
+}
